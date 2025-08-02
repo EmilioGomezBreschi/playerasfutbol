df2b2a60d28e84fb5b2f49d35fbd3601c70e9e84
EmilioGomezBreschi <emilio.gomez7881@alumnos.udg.mx>
2025-08-02 14:13:34 -0600

    Inicio de la apgina casi terminado
    TODO: Error en el espaciado del proyecto

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,119 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main className="flex min-h-screen flex-col items-center justify-start p-5">
+      <div className="items-center justify-center text-center mb-8">
+        <h1 className="text-4xl font-bold mb-3">Retro Clásicas</h1>
+        <h2 className="text-lg font-light">
+          Camisas históricas y vintage de épocas doradas del fútbol
+        </h2>
+      </div>
+      <div className="flex md:flex-row flex-col justify-around w-auto md:w-full md:space-x-7 space-y-7 md:space-y-0 grow">
+        <button
+          className="flex flex-col w-full rounded-lg border cursor-pointer min-h-full"
+          onClick={() => (window.location.href = "/retro")}
+        >
+          <div className="group flex-1 flex justify-center items-center md:h-full rounded-lg bg-red-500/70 hover:bg-red-500 transition-all duration-300">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/images/shirt.svg"
+              alt="Playera de Fútbol"
+              width={100}
+              height={100}
+              className="opacity-70 group-hover:opacity-100 group-hover:scale-115 transition-transform duration-300 ease-out"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <h3 className="text-start text-lg font-bold p-7">Camisas Retro</h3>
+        </button>
+        <button
+          className="flex flex-col w-full rounded-lg border cursor-pointer min-h-full"
+          onClick={() => (window.location.href = "/jugador")}
+        >
+          <div className="group flex-1 flex justify-center items-center md:h-full rounded-lg bg-yellow-500/70 hover:bg-yellow-500 transition-all duration-300">
+            <Image
+              src="/images/shirt.svg"
+              alt="Playera de Fútbol"
+              width={100}
+              height={100}
+              className="opacity-70 group-hover:opacity-100 group-hover:scale-115 transition-transform duration-300 ease-out"
+            />
+          </div>
+          <h3 className="text-start text-lg font-bold p-7">
+            Camisas de Jugador
+          </h3>
+        </button>
+        <button
+          className="flex flex-col w-full rounded-lg border cursor-pointer min-h-full"
+          onClick={() => (window.location.href = "/aficionado")}
+        >
+          <div className="group flex-1 flex justify-center items-center md:h-full rounded-lg bg-blue-900/70 hover:bg-blue-900 transition-all duration-300">
+            <Image
+              src="/images/shirt.svg"
+              alt="Playera de Fútbol"
+              width={100}
+              height={100}
+              className="opacity-70 group-hover:opacity-100 group-hover:scale-115 transition-transform duration-300 ease-out"
+            />
+          </div>
+          <h3 className="text-start text-lg font-bold p-7">
+            Camisas de Aficionado
+          </h3>
+        </button>
+      </div>
+      <footer className="mt-8 text-center text-sm text-black flex flex-row justify-around w-full">
+        <div className="flex items-center flex-col space-y-3">
+          <div className="w-15 h-15 bg-red-500/70 rounded-full flex justify-center items-center">
+            <Image
+              src="/images/shirtorange.svg"
+              alt="Playera de Fútbol"
+              width={40}
+              height={40}
+              className="opacity-70 group-hover:opacity-100 group-hover:scale-115 transition-transform duration-300 ease-out"
+            />
+          </div>
+          <h3 className="text-center text-base font-extrabold">
+            Retro Auténticas
+          </h3>
+          <p className="text-center text-sm font-bold text-gray-900">
+            Camisas históricas con diseños originales y materiales vintage.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div className="flex items-center flex-col space-y-3">
+          <div className="w-15 h-15 bg-yellow-500/70 rounded-full flex justify-center items-center">
+            <Image
+              src="/images/star.svg"
+              alt="Playera de Fútbol"
+              width={40}
+              height={40}
+              className="opacity-70 group-hover:opacity-100 group-hover:scale-115 transition-transform duration-300 ease-out"
+            />
+          </div>
+          <h3 className="text-center text-base font-extrabold">
+            Calidad Jugador
+          </h3>
+          <p className="text-center text-sm font-bold text-gray-900">
+            Misma calidad que usan los profesionales en el campo.
+          </p>
+        </div>
+        <div className="flex items-center flex-col space-y-3">
+          <div className="w-15 h-15 bg-blue-900/70 rounded-full flex justify-center items-center">
+            <Image
+              src="/images/heart.svg"
+              alt="Playera de Fútbol"
+              width={40}
+              height={40}
+              className="opacity-70 group-hover:opacity-100 group-hover:scale-115 transition-transform duration-300 ease-out"
+            />
+          </div>
+          <h3 className="text-center text-base font-extrabold">
+            Para aficionados
+          </h3>
+          <p className="text-center text-sm font-bold text-gray-900">
+            Diseños accesibles sin comprometer el estilo y la comodidad.
+          </p>
+        </div>
       </footer>
-    </div>
+    </main>
   );
 }
